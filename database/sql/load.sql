68270e47ebcc915d0c2efbbc5d5312abadd89e57
Kevin Li <lku@kenun.li>
2022-04-20 10:11:57 -0400

    Modified `load.sql` to load additional tables (`vehicle` and `person`)

--- database/sql/load.sql
+++ database/sql/load.sql
@@ -1,1 +1,3 @@
 COPY collision FROM '/docker-entrypoint-initdb.d/collision.csv' CSV HEADER;
+COPY vehicle FROM '/docker-entrypoint-initdb.d/vehicle.csv' CSV HEADER;
+COPY person FROM '/docker-entrypoint-initdb.d/person.csv' CSV HEADER;
