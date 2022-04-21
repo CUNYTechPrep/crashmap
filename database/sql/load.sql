a4ab421715ff013c4ad08b59cde6637fe93993ce
Kevin Li <lku@kenun.li>
2022-04-21 00:28:41 -0400

    Added shapes for boroughs and neighborhoods to database

--- database/sql/load.sql
+++ database/sql/load.sql
@@ -1,3 +1,5 @@
+COPY boro FROM '/docker-entrypoint-initdb.d/boro.csv' CSV HEADER;
+COPY nta FROM '/docker-entrypoint-initdb.d/nta.csv' CSV HEADER;
 COPY collision FROM '/docker-entrypoint-initdb.d/collision.csv' CSV HEADER;
 COPY vehicle FROM '/docker-entrypoint-initdb.d/vehicle.csv' CSV HEADER;
 COPY person FROM '/docker-entrypoint-initdb.d/person.csv' CSV HEADER;
