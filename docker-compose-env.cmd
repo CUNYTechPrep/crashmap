a223fa398fcdd5bdb79db3fdbfb47e73154d6992
Kevin Li <lku@kenun.li>
2022-04-24 02:17:49 -0400

    Added scripts for running `docker-compose` commands with a specific set of environment variables (e.g., “staging”)

---
+++ docker-compose-env.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+call:loadEnvs .env .env.%1 .env.local .env.%1.local
+docker-compose --env-file "" -f "docker-compose.%1.yml" "%2"
+exit /B %errorlevel%
+endlocal
+
+:loadEnvs
+:loadEnv
+if "%1" == "" exit /B 0
+if exist %1 (
+    for /f "eol=# tokens=*" %%i in (%1) do set %%i
+    echo Loaded environment variables from %1.
+) else (
+    echo %1 not found.
+)
+shift
+goto loadEnv
---
+++ docker-compose-env.sh
@@ -0,0 +1,23 @@
+loadEnv() {
+  local envFile="${1}"
+  if [ -f "$envFile" ]; then
+    local environmentAsArray variableDeclaration
+    mapfile environmentAsArray < <(
+      grep --invert-match '^#' "${envFile}" | # Remove commented lines.
+        grep --invert-match '^\s*$' # Remove blank lines.
+    )
+    for variableDeclaration in "${environmentAsArray[@]}"; do
+      export "${variableDeclaration//[$'\r\n']/}" # Remove line breaks and export the variables.
+    done
+    echo "Loaded environment variables from $envFile."
+  else
+    echo "$envFile not found."
+  fi
+}
+
+loadEnv .env
+loadEnv ".env.$1"
+loadEnv .env.local
+loadEnv ".env.$1.local"
+
+docker-compose --env-file '' -f "docker-compose.$1.yml" "$2"
