20a82c955cfea5f9b9da58bb10aa3d7d5e5e2da9
Kevin Li <lku@kenun.li>
2022-04-24 22:31:49 -0400

    Removed unnecessary space characters

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class BoroModel(db.Model):
     __tablename__ = 'boro'
     the_geom = ga2.Column(ga2.Geography('MULTIPOLYGON'), nullable=False)
-    boro_code = db.Column(db.INTEGER(), nullable=False, primary_key = True)
+    boro_code = db.Column(db.INTEGER(), nullable=False, primary_key=True)
     boro_name = db.Column(db.VARCHAR(), nullable=False)
     shape_leng = db.Column(db.REAL(), nullable=False)
     shape_area = db.Column(db.REAL(), nullable=False)
