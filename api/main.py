8697268680be89b4690bbbce6bb95a4180c192cb
Kevin Li <lku@kenun.li>
2022-04-24 22:13:09 -0400

    Removed unnecessary `after_request` handler after proxying API requests originating through the React frontend

--- api/main.py
+++ api/main.py
@@ -39,19 +39,6 @@ def create_app() -> Flask:  # TODO: Move views to a separate file
         response.headers['Content-type'] = 'application/geo+json' if is_geojson else 'application/json'
         return response
 
-    # See the Stack Overflow answer for why this is needed: https://stackoverflow.com/a/44572672/1405571.
-    @app.after_request
-    def add_cors_headers(response: Response) -> Response:
-        if request.referrer and url_parse(request.referrer[:-1]).host == request.host:
-            response.headers.add('Access-Control-Allow-Origin', request.host_url)
-            response.headers.add('Access-Control-Allow-Credentials', 'true')
-            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-            response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
-            response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
-            response.headers.add('Access-Control-Allow-Headers', 'Authorization')
-            response.headers.add('Access-Control-Allow-Methods', 'GET, OPTIONS, POST')
-        return response
-
     @app.errorhandler(HTTPException)
     def errorhandler(exception: HTTPException):
         if app.debug:
