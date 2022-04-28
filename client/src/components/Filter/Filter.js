58b2a8f1e25d8565dc1688fffec5e35da4754833
Javier M <73079627+JavierMajano@users.noreply.github.com>
2022-04-27 23:22:46 -0400

    Added functionally to filter with boroughs

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
+import Select from 'react-select';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./styles.css";
 
+
+
+const options = [
+  { value: 'Bronx', label: 'BRONX' },
+  { value: 'Brooklyn', label: 'BROOKLYN' },
+  { value: 'Queens', label: 'QUEENS' },
+  { value: 'Manhattan', label: 'MANHATTAN' },
+  { value: 'Staten Island', label: 'STATEN ISLAND' },
+]
 class Filter extends Component {
+  state ={
+    selectedOption: null
+  }
+  handleChange = (selectedOption) => {
+    console.log(selectedOption.value)
+    //this.setState({radius: selectedOption.value})
+    this.setState({ selectedOption, }, () =>
+      console.log(`Option selected:`, this.state.selectedOption.label)
+
+    );
+  };
+
+   
+  currentBorough = (event) => { 
+    console.log(this.state.selectedOption)
+  }
   render() {
+    const { selectedOption } = this.state;
     return (
+      
       <Row id="filter">
         <Col xs={2}>
-          <select name="" id="">
-            <option value="" disabled selected hidden>
-              Borough
-            </option>
-            <option value="manhattan">Manhattan</option>
-            <option value="brooklyn">Brooklyn</option>
-            <option value="queens">Queens</option>
-            <option value="bronx">Bronx</option>
-            <option value="staten island">Staten Island</option>
-          </select>
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
+            <button className="btn btn-primary" onClick={this.currentBorough}>Enter city</button>
         </Col>
         <Col xs={2}>
           <select name="" id="" className="w-5">
@@ -26,6 +50,7 @@ class Filter extends Component {
             </option>
           </select>
         </Col>
+    
       </Row>
     );
   }
