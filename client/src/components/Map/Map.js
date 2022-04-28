955837bb29a01bd24893dd9a72a6a18fb23e8b8a
Sean Yap <syap0317@gmail.com>
2022-04-27 23:11:42 -0400

    added functionality to generate geojson object for neighborhoods in each borough

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import Row from "react-bootstrap/Row";
 
 import boroughs from "./data/borough_boundaries.geojson";
+import neighborhoods from "./data/2020_neighborhood_tabulation.geojson";
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic2VhbnlhcCIsImEiOiJjbDFmZnJjaHoxMTJ6M29zOXRoajZ0czlvIn0.BJC-KwTeHSCklDlBG9SEuQ";
@@ -21,6 +22,68 @@ class Map extends Component {
   }
 
   componentDidMount() {
+    let bronxNeighborhoodGeojson = {
+      name: "Bronx",
+      type: "FeatureCollection",
+      features: [],
+    };
+    let brooklynNeighborhoodGeojson = {
+      name: "Brooklyn",
+      type: "FeatureCollection",
+      features: [],
+    };
+    let manhattanNeighborhoodGeojson = {
+      name: "Manhattan",
+      type: "FeatureCollection",
+      features: [],
+    };
+    let queensNeighborhoodGeojson = {
+      name: "Queens",
+      type: "FeatureCollection",
+      features: [],
+    };
+    let statenIslandNeighborhoodGeojson = {
+      name: "StatenIsland",
+      type: "FeatureCollection",
+      features: [],
+    };
+
+    fetch(neighborhoods)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        data.features.forEach((neighborhood) => {
+          const borough = neighborhood.properties.boroname;
+          // insert borough into geojson object
+          switch (borough) {
+            case "Bronx":
+              bronxNeighborhoodGeojson.features.push(neighborhood);
+              break;
+            case "Brooklyn":
+              brooklynNeighborhoodGeojson.features.push(neighborhood);
+              console.log("brooklyn");
+              break;
+            case "Manhattan":
+              manhattanNeighborhoodGeojson.features.push(neighborhood);
+              break;
+            case "Queens":
+              queensNeighborhoodGeojson.features.push(neighborhood);
+              break;
+            case "Staten Island":
+              statenIslandNeighborhoodGeojson.features.push(neighborhood);
+              break;
+            default:
+              console.log("Borough Not Read: " + borough);
+          }
+        });
+        // console.log(bronxNeighborhoodGeojson);
+        // console.log(brooklynNeighborhoodGeojson);
+        // console.log(manhattanNeighborhoodGeojson);
+        // console.log(queensNeighborhoodGeojson);
+        // console.log(statenIslandNeighborhoodGeojson);
+      });
+
     const { lng, lat, zoom } = this.state;
     const map = new mapboxgl.Map({
       container: this.mapContainer.current,
