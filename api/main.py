c9ab89edf86c93bab5700d92df6efbf307e51895
Kevin Li <lku@kenun.li>
2022-05-02 10:58:24 -0400

    Factored out code to the `service` module and added support for querying multiple H3 cells centered around a given H3 cell

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 from datetime import date, time
 from os import getenv
 from flask import Flask, jsonify, make_response, request, Response
-from werkzeug.exceptions import BadRequest
 
-from models import Boro, BoroSummary, Collision, db, H3, H3Summary, NTA2020, NTA2020Summary, Person, Summary, Vehicle
-from services import CustomEncoder
+from models import db
+from services import BoroService, CollisionService, CustomEncoder, H3Service, NTA2020Service, SummaryService
 
 
 def create_app() -> Flask:  # TODO: Move views to a separate file
@@ -17,8 +16,6 @@ def create_app() -> Flask:  # TODO: Move views to a separate file
     app.config['SQLALCHEMY_DATABASE_URI'] = '{SCHEME}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**db_uri)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
-    # TODO: Initialize services here.
 
     def make_geojson_response(obj: dict | list) -> Response:
         response = make_response({'type': 'FeatureCollection',
@@ -34,67 +31,37 @@ def create_app() -> Flask:  # TODO: Move views to a separate file
 
     @app.route('/api/boro.geojson', methods=['GET'])
     def boro_as_geojson() -> Response:
-        query = db.session.query(Boro)
-        if (id := request.args.get('id', None, int)) is not None:
-            query = query.filter(Boro.id == id)
-        return make_geojson_response(query.all())
+        id = request.args.get('id', None, int)
+        return make_geojson_response(BoroService.get_boro(id))
 
     @app.route('/api/nta2020.geojson', methods=['GET'])
     def nta2020_as_geojson() -> Response:
-        query = db.session.query(NTA2020)
-        if (id := request.args.get('id', None, str)) is not None:
-            query = query.filter(NTA2020.id == id)
-        if (boro_id := request.args.get('boro_id', None, int)) is not None:
-            query = query.filter(NTA2020.boro_id == boro_id)
-        return make_geojson_response(query.all())
+        id = request.args.get('id', None, str)
+        boro_id = request.args.get('boro_id', None, int)
+        return make_geojson_response(NTA2020Service.get_nta2020(id, boro_id))
 
     @app.route('/api/h3.geojson', methods=['GET'])
     def h3_as_geojson() -> Response:
-        query = db.session.query(H3)
-        if (h3_index := request.args.get('h3_index', None, int)) is not None:
-            query = query.filter(H3.h3_index == h3_index)
-        if (nta2020_id := request.args.get('nta2020_id', None, int)) is not None:
-            query = query.filter(H3.nta2020s.any(id=nta2020_id))
-        if (boro_id := request.args.get('boro_id', None, int)) is not None:
-            query = query.filter(H3.nta2020s.any(boro_id=boro_id))
-        return make_geojson_response(query.all())
+        h3_index = request.args.get('h3_index', None, int)
+        k = request.args.get('k', None, int)
+        nta2020_id = request.args.get('nta2020_id', None, str)
+        boro_id = request.args.get('boro_id', None, int)
+        only_water = request.args.get('only_water', None, bool)
+        return make_geojson_response(H3Service.get_h3(h3_index, k, nta2020_id, boro_id, only_water))
 
     @app.route('/api/collision.geojson', methods=['GET'])
     def collision_as_geojson() -> Response:
-        query = db.session.query(Collision)
-        return jsonify(query.all())
+        id = request.args.get('id', None, int)
+        return jsonify(CollisionService.get_collision(id))
 
     @app.route('/api/summary.json', methods=['GET'])
     def summary_as_json() -> Response:
         h3_index = request.args.get('h3_index', None, int)
-        nta2020_id = request.args.get('nta2020_id', None, int)
+        nta2020_id = request.args.get('nta2020_id', None, str)
         boro_id = request.args.get('boro_id', None, int)
-        if h3_index is not None and nta2020_id is not None or \
-           h3_index is not None and boro_id is not None or \
-           nta2020_id is not None and boro_id is not None:
-            raise BadRequest('h3_index, nta2020_id, and boro_id are mutually exclusive parameters.')
-        if h3_index is not None:
-            model = H3Summary
-            query = db.session.query(model) \
-                              .filter(H3Summary.h3_index == h3_index)
-        elif nta2020_id is not None:
-            model = NTA2020Summary
-            query = db.session.query(model) \
-                              .filter(NTA2020Summary.nta2020_id == nta2020_id)
-        elif boro_id is not None:
-            model = BoroSummary
-            query = db.session.query(model) \
-                              .filter(BoroSummary.boro_id == boro_id)
-        else:
-            model = Summary
-            query = db.session.query(Summary)
         start_date = request.args.get('start_date', None, date.fromisoformat)
         end_date = request.args.get('end_date', None, date.fromisoformat)
-        if start_date is not None:
-            query = query.filter(model.date >= start_date)
-        if end_date is not None:
-            query = query.filter(model.date <= end_date)
-        return jsonify(query.all())
+        return jsonify(SummaryService.get_summary(h3_index, nta2020_id, boro_id, start_date, end_date))
 
     return app
 
--- api/services.py
+++ api/services.py
@@ -3,6 +3,28 @@ from datetime import date, time
 from flask.json import JSONEncoder
 from geoalchemy2 import WKBElement
 from geoalchemy2.shape import to_shape
+from h3 import h3_to_string, k_ring, string_to_h3
+from typing import Optional
+
+from models import Boro, BoroSummary, Collision, db, H3, H3Summary, NTA2020, NTA2020Summary, Person, Summary, Vehicle
+
+
+class BoroService:
+    @staticmethod
+    def get_boro(id: Optional[int]) -> list[Boro]:
+        query = db.session.query(Boro)
+        if id is not None:
+            query = query.filter(Boro.id == id)
+        return query.all()
+
+
+class CollisionService:
+    @staticmethod
+    def get_collision(id: Optional[int]) -> list[Collision]:
+        query = db.session.query(Collision)
+        if id is not None:
+            query = query.filter(Collision.id == id)
+        return query.all()
 
 
 class CustomEncoder(JSONEncoder):
@@ -14,3 +36,69 @@ class CustomEncoder(JSONEncoder):
         elif isinstance(o, WKBElement):
             return to_shape(o).__geo_interface__
         return super().default(o)
+
+
+class H3Service:
+    @staticmethod
+    def get_h3(h3_index: Optional[int], k: Optional[int],
+               nta2020_id: Optional[str], boro_id: Optional[int], only_water: Optional[bool]) \
+            -> list[H3]:
+        query = db.session.query(H3)
+        if h3_index is not None:
+            if k is None or k == 0:
+                query = query.filter(H3.h3_index == h3_index)
+            else:
+                if k < 0 or k > 6:  # 1 cell at resolution 9 is 0.1 km². k == 6 results in a set of 127 cells—13.5 km².
+                    raise ValueError('k must be a non-negative integer less than 7.')
+                query = query.filter(H3.h3_index.in_(map(string_to_h3, k_ring(h3_to_string(h3_index), k))))
+        elif k is not None:
+            raise ValueError('k cannot be specified without h3_index.')
+        if nta2020_id is not None:
+            query = query.filter(H3.nta2020s.any(id=nta2020_id))
+        if boro_id is not None:
+            query = query.filter(H3.nta2020s.any(boro_id=boro_id))
+        if only_water is not None:
+            query = query.filter(H3.only_water == only_water)
+        return query.all()
+
+
+class NTA2020Service:
+    @staticmethod
+    def get_nta2020(id: Optional[str], boro_id: Optional[int]) -> list[NTA2020]:
+        query = db.session.query(NTA2020)
+        if id is not None:
+            query = query.filter(NTA2020.id == id)
+        if boro_id is not None:
+            query = query.filter(NTA2020.boro_id == boro_id)
+        return query.all()
+
+
+class SummaryService:
+    @staticmethod
+    def get_summary(h3_index: Optional[int], nta2020_id: Optional[str], boro_id: Optional[int],
+                    start_date: Optional[date], end_date: Optional[date]) \
+            -> list[BoroSummary | H3Summary | NTA2020Summary]:
+        if h3_index is not None and nta2020_id is not None or \
+           h3_index is not None and boro_id is not None or \
+           nta2020_id is not None and boro_id is not None:
+            raise ValueError('h3_index, nta2020_id, and boro_id are mutually exclusive arguments.')
+        if h3_index is not None:
+            model = H3Summary
+            query = db.session.query(model) \
+                              .filter(H3Summary.h3_index == h3_index)
+        elif nta2020_id is not None:
+            model = NTA2020Summary
+            query = db.session.query(model) \
+                              .filter(NTA2020Summary.nta2020_id == nta2020_id)
+        elif boro_id is not None:
+            model = BoroSummary
+            query = db.session.query(model) \
+                              .filter(BoroSummary.boro_id == boro_id)
+        else:
+            model = Summary
+            query = db.session.query(Summary)
+        if start_date is not None:
+            query = query.filter(model.date >= start_date)
+        if end_date is not None:
+            query = query.filter(model.date <= end_date)
+        return query.all()
