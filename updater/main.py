2012e7996d0693974af9e80d773cd6287a856afc
Kevin Li <lku@kenun.li>
2022-05-06 08:50:54 -0400

    Initial commit of `updater`

---
+++ updater/main.py
@@ -0,0 +1,160 @@
+from datetime import date
+from functools import partial
+import geopandas as gpd
+from geopandas import GeoDataFrame
+import lzma
+import numpy as np
+from os import getenv
+import pandas as pd
+from pandas import DataFrame, Series
+from shapely.geometry.base import BaseGeometry
+from smart_open import register_compressor, smart_open
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from toolz import compose_left as compose, get, pipe, valmap
+from typing import Any, Callable, Optional, Sequence
+from urllib.parse import urlencode
+
+register_compressor('.xz', partial(lzma.LZMAFile, format=lzma.FORMAT_XZ))
+
+
+series_to_postgresql_array_literal: Callable[[Series], str | float] = \
+    compose(Series.dropna,
+            partial(map, lambda value: value.replace('\'', '\'\'') if type(value) is str else value),
+            partial(map, '\'{}\''.format),
+            ', '.join,
+            '{{{}}}'.format,
+            lambda result: np.nan if result == '{}' else result)
+
+
+def combine_columns(data_frame: DataFrame, source_columns: Sequence, aggregation_function: Callable[[Series], Any],
+                    new_column_name: str) -> DataFrame:
+    columns_to_aggregate = data_frame.get(source_columns)
+    result = data_frame.assign(**{new_column_name: columns_to_aggregate.agg(aggregation_function, axis=1)})
+    result = result.drop(columns=source_columns)
+    return result
+
+
+nyc_geometry_path = 'resources/nyc_area.gpkg.xz'
+urls = {'collision': 'https://data.cityofnewyork.us/resource/h9gi-nx95.csv',
+        'vehicle': 'https://data.cityofnewyork.us/resource/bm4k-52h4.csv',
+        'person': 'https://data.cityofnewyork.us/resource/f55k-p6yu.csv'}
+column_name_maps = {'collision': {'collision_id': 'id',
+                                   'crash_date': 'date',
+                                   'crash_time': 'time',
+                                   'latitude': 'latitude',
+                                   'longitude': 'longitude'},
+                    'vehicle': {'unique_id': 'id',
+                                 'collision_id': 'collision_id',
+                                 'state_registration': 'state_registration',
+                                 'vehicle_type': 'type',
+                                 'vehicle_make': 'make',
+                                 'vehicle_model': 'model',
+                                 'vehicle_year': 'year',
+                                 'travel_direction': 'travel_direction',
+                                 'vehicle_occupants': 'occupants',
+                                 'driver_sex': 'driver_sex',
+                                 'driver_license_status': 'driver_license_status',
+                                 'driver_license_jurisdiction': 'driver_license_jurisdiction',
+                                 'pre_crash': 'pre_crash',
+                                 'point_of_impact': 'point_of_impact',
+                                 'vehicle_damage_1': 'damage_1',
+                                 'vehicle_damage_2': 'damage_2',
+                                 'vehicle_damage_3': 'damage_3',
+                                 'public_property_damage': 'public_property_damage',
+                                 'public_property_damage_type': 'public_property_damage_type',
+                                 'contributing_factor_1': 'contributing_factor_1',
+                                 'contributing_factor_2': 'contributing_factor_2'},
+                    'person': {'unique_id': 'id',
+                               'collision_id': 'collision_id',
+                               'vehicle_id': 'vehicle_id',
+                               'person_type': 'type',
+                               'person_injury': 'injury',
+                               'person_age': 'age',
+                               'ejection': 'ejection',
+                               'emotional_status': 'emotional_status',
+                               'bodily_injury': 'bodily_injury',
+                               'position_in_vehicle': 'position_in_vehicle',
+                               'safety_equipment': 'safety_equipment',
+                               'ped_location': 'location',
+                               'ped_action': 'action',
+                               'complaint': 'complaint',
+                               'ped_role': 'role',
+                               'contributing_factor_1': 'contributing_factor_1',
+                               'contributing_factor_2': 'contributing_factor_2',
+                               'person_sex': 'sex'}}
+url_parameters = valmap(lambda column_name_map: {'$select': ','.join(column_name_map), '$limit': '100000'},
+                        column_name_maps)
+
+
+def get_nyc_geometry(uri: str) -> BaseGeometry:
+    with smart_open(uri) as file:
+        return gpd.read_file(file).iloc[0].iat[0]
+
+
+def create_database_engine() -> Engine:
+    db_uri = {component: getenv(f'DATABASE_{component}')
+              for component in ('SCHEME', 'HOST', 'PORT', 'USERNAME', 'PASSWORD', 'NAME')}
+    return create_engine('{SCHEME}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**db_uri))
+
+
+def get_last_append_date(database_engine: Engine) -> Optional[date]:
+    connection = database_engine.connect()
+    try:
+        return connection.execute('SELECT max(collision.date) FROM collision') \
+                         .scalar_one_or_none()
+    finally:
+        connection.close()
+
+
+def get_new_data(start: date) -> dict[str, DataFrame]:
+    return {data_set: pipe('%s?%s' % (url, urlencode(url_parameters[data_set] | {'$where': f'\'{start}\'<crash_date'})),
+                           partial(pd.read_csv, dtype='str'),
+                           partial(DataFrame.rename, columns=column_name_maps[data_set]))
+            for data_set, url in urls.items()}
+
+
+def transform_data_sets(data_sets: dict[str, DataFrame], nyc_geometry: BaseGeometry) -> dict[str, DataFrame]:
+    collision, vehicle, person = map(compose(partial(DataFrame.set_index, keys='id'), DataFrame.copy),
+                                       get(['collision', 'vehicle', 'person'], data_sets))
+    valid_locations = GeoDataFrame(collision, geometry=gpd.points_from_xy(collision.longitude, collision.latitude)) \
+                     .within(nyc_geometry)
+    collision.latitude = np.where(valid_locations, collision.latitude, np.nan)
+    collision.longitude = np.where(valid_locations, collision.longitude, np.nan)
+    collision = collision.drop(columns=['geometry'])
+    vehicle = combine_columns(vehicle, ['damage_1', 'damage_2', 'damage_3'],
+                               series_to_postgresql_array_literal, 'damages')
+    vehicle = combine_columns(vehicle, ['contributing_factor_1', 'contributing_factor_2'],
+                               series_to_postgresql_array_literal, 'contributing_factors')
+    person = combine_columns(person, ['contributing_factor_1', 'contributing_factor_2'],
+                             series_to_postgresql_array_literal, 'contributing_factors')
+    person['dangling_vehicle_id'] = np.where(person.vehicle_id.isin(vehicle.index), np.nan, person.vehicle_id)
+    person.vehicle_id = np.where(person.vehicle_id.isin(vehicle.index), person.vehicle_id, np.nan)
+    return dict(zip(('collision', 'vehicle', 'person'),
+                    (collision, vehicle, person)))
+
+
+def load_data(database_engine: Engine, data_set: dict[str, DataFrame]) -> None:
+    connection = database_engine.connect()
+    try:
+        for table_name, data_frame in data_set.items():
+            data_frame.to_sql(table_name, connection, if_exists='append', method='multi')
+    finally:
+        connection.close()
+
+
+def run():
+    nyc_geometry = get_nyc_geometry(nyc_geometry_path)
+    database_engine = create_database_engine()
+
+    try:
+        last_append_date = get_last_append_date(database_engine)
+        data_sets = get_new_data(last_append_date)
+        transformed_data_sets = transform_data_sets(data_sets, nyc_geometry)
+        load_data(database_engine, transformed_data_sets)
+    finally:
+        database_engine.dispose()
+
+
+if __name__ == '__main__':
+    run()
---
+++ updater/run.development.sh
@@ -0,0 +1,30 @@
+##!/usr/local/Cellar/bash/5.1.16/bin/bash
+
+# Note: If you are running this script with bash, you will need bash version 5.
+#   This is crucial especially for macOS user because macOS ships by default
+#   bash version 3 at the time of writing this.
+# Example shebang on line 1 if you installed bash 5.1.16 with brew. Uncomment 
+#   and modify it to the path you installed bash 5.
+
+loadEnv() {
+  local envFile="${1}"
+  if [ -f "$envFile" ]; then
+    local environmentAsArray variableDeclaration
+    mapfile environmentAsArray < <(
+      grep --invert-match '^#' "${envFile}" | # Remove commented lines.
+        grep --invert-match '^\s*$' # Remove blank lines.
+    )
+    for variableDeclaration in "${environmentAsArray[@]}"; do
+      export "${variableDeclaration//[$'\r\n']/}" # Remove line breaks and export the variables.
+    done
+    echo "Loaded environment variables from $envFile."
+  else
+    echo "$envFile not found."
+  fi
+}
+
+loadEnv ../.env
+loadEnv ../.env.development
+loadEnv ../.env.local
+loadEnv ../.env.development.local
+python main.py
