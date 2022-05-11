64a1ea72b7a6b7fb44e64e599b413ac987172181
Sean Yap <syap0317@gmail.com>
2022-05-11 14:56:55 -0400

    changed borough names to remove capital letter and spaces

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import Select from 'react-select';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './styles.css';
+import React, { Component } from "react";
+import Select from "react-select";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./styles.css";
 
-const options = [
-  { value: 'Bronx', label: 'BRONX', lng: '-73.865433', lat: '40.8448' },
-  { value: 'Brooklyn', label: 'BROOKLYN', lng: '-73.9442', lat: '40.6782' },
-  { value: 'Queens', label: 'QUEENS', lng: '-73.7949', lat: '40.7282' },
-  { value: 'Manhattan', label: 'MANHATTAN', lng: '-73.9712', lat: '40.7831' },
+const boroughOptions = [
+  { value: "bronx", label: "BRONX", lng: "-73.865433", lat: "40.8448" },
+  { value: "brooklyn", label: "BROOKLYN", lng: "-73.9442", lat: "40.6782" },
+  { value: "queens", label: "QUEENS", lng: "-73.7949", lat: "40.7282" },
+  { value: "manhattan", label: "MANHATTAN", lng: "-73.9712", lat: "40.7831" },
   {
-    value: 'Staten Island',
-    label: 'STATEN ISLAND',
-    lng: '-74.1502',
-    lat: '40.5795',
+    value: "statenIsland",
+    label: "STATEN ISLAND",
+    lng: "-74.1502",
+    lat: "40.5795",
   },
 ];
 
@@ -24,23 +24,24 @@ class Filter extends Component {
 
   currentBorough = (event) => {
     if (this.props.selectedBorough)
-      console.log('inside currentBorough: ' + this.props.selectedBorough.value);
-    else console.log('inside currentBorough: nothing selected!');
+      console.log("inside currentBorough: " + this.props.selectedBorough.value);
+    else console.log("inside currentBorough: nothing selected!");
   };
 
   render() {
     return (
-      <Row id='filter'>
+      <Row id="filter">
         <Col xs={2}>
           {/* Borough Dropdown */}
           <Select
             value={this.props.selectedBorough}
             onChange={this.props.handleBoroughChange}
-            options={options}
+            options={boroughOptions}
           />
+          {/* TODO: include h3 borough options */}
         </Col>
         <Col xs={2}>
-          <button className='btn btn-primary' onClick={this.currentBorough}>
+          <button className="btn btn-primary" onClick={this.currentBorough}>
             Enter city
           </button>
 
