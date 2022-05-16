9a5194e589c339828e079490e0231d46244d4855
Kevin Li <lku@kenun.li>
2022-05-15 21:46:32 -0400

    Cancelled out the effect of previous reversion of a reversion to code under `client`

--- client/src/components/BarChart/BarChart.js
+++ client/src/components/BarChart/BarChart.js
@@ -1,192 +1,267 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+// import React, { useEffect, useState } from 'react';
+// import { Bar } from 'react-chartjs-2';
+// import { Chart as ChartJS, BarElement } from 'chart.js';
 
-class BarChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      labels: [],
-      borough: [],
-      data: [],
-    };
-  }
+// ChartJS.register(BarElement);
 
-  componentDidMount() {
-    const api_prefix = process.env.REACT_APP_API_PROXY_URL_PREFIX;
+// function BarChart(props) {
+//   const [chart, setChart] = useState({});
 
-    let arr2 = [];
-    let arr3 = [];
-    fetch(
-      `${api_prefix}boro_summary.json?boro_id=1&start_date=2020-01-01&end_date=2020-01-31`
-    )
-      .then(function (res) {
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        arr3 = {
-          id: '1',
-          collision: json[0].collisions,
-          name: 'Manhattan',
-        };
-        arr2.push(arr3);
-        // console.log(arr3);
-      });
-    fetch(
-      `${api_prefix}boro_summary.json?boro_id=2&start_date=2020-01-01&end_date=2020-01-31`
-    )
-      .then(function (res) {
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        arr3 = {
-          id: '2',
-          collision: json[0].collisions,
-          name: 'Bronx',
-        };
-        arr2.push(arr3);
-        // console.log(arr2);
-      });
-    fetch(
-      `${api_prefix}boro_summary.json?boro_id=3&start_date=2020-01-01&end_date=2020-01-31`
-    )
-      .then(function (res) {
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        arr3 = {
-          id: '3',
-          collision: json[0].collisions,
-          name: 'Brooklyn',
-        };
-        arr2.push(arr3);
-        // console.log(arr2);
-      });
-    fetch(
-      `${api_prefix}boro_summary.json?boro_id=4&start_date=2020-01-01&end_date=2020-01-31`
-    )
-      .then(function (res) {
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        arr3 = {
-          id: '4',
-          collision: json[0].collisions,
-          name: 'Queens',
-        };
-        arr2.push(arr3);
-      });
+//   const api_prefix = process.env.REACT_APP_API_PROXY_URL_PREFIX;
 
-    fetch(
-      `${api_prefix}boro_summary.json?boro_id=5&start_date=2020-01-01&end_date=2020-01-31`
-    )
-      .then(function (res) {
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        arr3 = {
-          id: '5',
-          collision: json[0].collisions,
-          name: 'Staten Island',
-        };
-        arr2.push(arr3);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       await fetch(`${api_prefix}boro_summary.json`)
+//         .then((response) => {
+//           if (response.ok) {
+//             response.json().then((data) => {
+//               // console.log(json.data);
+//               setChart(data);
+//             });
+//           }
+//         })
+//         .catch((error) => {
+//           console.log(error);
+//         });
+//     };
+//     fetchData();
+//   }, [api_prefix]);
 
-        // console.log(this.state.borough);
-        let obj = [];
-        let obj3 = [];
+//   console.log('chart', chart);
+//   var data = {
+//     labels: ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'Staten Island'],
+//     datasets: [
+//       {
+//         label: 'Collisions',
+//         data: chart?.map((x) => x.collisions),
+//         // data: [1, 2, 3, 4, 5],
+//         backgroundColor: [
+//           'rgba(100,178,71,0.2)', //MN
+//           'rgba(192,120,98, 0.2)', //BX
+//           'rgba(98,	123, 193, 0.2)', //BK
+//           'rgba(192, 98, 170, 0.2)', //QN
+//           'rgba(192, 167,	98, 0.2)', //SI
+//         ],
+//         borderColor: [
+//           'rgba(100,178,71, 1)',
+//           'rgba(192,120,98, 1)',
+//           'rgba(98,	123, 193, 1)',
+//           'rgba(192, 98, 170, 1)',
+//           'rgba(192, 167,	98, 1)',
+//         ],
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+//   // render() {
+//   return (
+//     <div>
+//       <Bar
+//         data={data}
+//         height={400}
+//         width={600}
+//         options={{
+//           maintainAspectRatio: false,
+//         }}
+//       />
+//       {/* <div>{this.state.chart}</div> */}
+//     </div>
+//   );
+//   // }
+// }
 
-        // console.log(obj3);
-        // console.log(obj);
-        // console.log(arr2);
-        this.setState({
-          borough: arr2,
+// export default BarChart;
+
+// import React, { useState, useEffect } from 'react';
+// import { Chart as ChartJS, BarElement } from 'chart.js';
+
+// import { Bar } from 'react-chartjs-2';
+
+// // ChartJS.register(BarElement);
+
+// function BarChart() {
+//   // const [chart, setChart] = useState({});
+//   // // var baseUrl = "https://api.coinranking.com/v2/coins/?limit=10";
+//   // // var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   // // var apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+//   // const api_prefix = process.env.REACT_APP_API_PROXY_URL_PREFIX;
+
+//   // useEffect(() => {
+//   //   const fetchData = async () => {
+//   //     await fetch(`${api_prefix}boro_summary.json`)
+//   //       .then((res) => {
+//   //         res.json();
+//   //         // if (response.ok) {
+//   //         //   response.json().then((json) => {
+//   //         //     console.log('here');
+//   //         //     // setChart(json);
+//   //         //   });
+//   //         // }
+//   //       })
+//   //       .then((data) => console.log(data))
+//   //       .catch((error) => {
+//   //         console.log(error);
+//   //       });
+//   //   };
+//   //   fetchData();
+//   // }, [api_prefix]);
+
+//   // console.log('chart', chart);
+//   // var data = {
+//   //   labels: ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'Staten Island'],
+//   //   datasets: [
+//   //     {
+//   //       label: 'Collisions',
+//   //       // data: chart?.map((x) => x.collisions),
+//   //       data: [1, 2, 3, 4, 5],
+//   //       backgroundColor: [
+//   //         'rgba(100,178,71,0.2)', //MN
+//   //         'rgba(192,120,98, 0.2)', //BX
+//   //         'rgba(98,	123, 193, 0.2)', //BK
+//   //         'rgba(192, 98, 170, 0.2)', //QN
+//   //         'rgba(192, 167,	98, 0.2)', //SI
+//   //       ],
+//   //       borderColor: [
+//   //         'rgba(100,178,71, 1)',
+//   //         'rgba(192,120,98, 1)',
+//   //         'rgba(98,	123, 193, 1)',
+//   //         'rgba(192, 98, 170, 1)',
+//   //         'rgba(192, 167,	98, 1)',
+//   //       ],
+//   //       borderWidth: 1,
+//   //     },
+//   //   ],
+//   // };
+
+//   return (
+//     <div>
+//       <Bar
+//         data={{
+//           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//           datasets: [
+//             {
+//               label: '# of votes',
+//               data: [12, 19, 3, 5, 2, 3],
+//               backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)',
+//               ],
+//               borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//               ],
+//               borderWidth: 1,
+//             },
+//           ],
+//         }}
+//         height={400}
+//         width={600}
+//         options={{
+//           maintainAspectRatio: false,
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default BarChart;
+
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+
+const BarChart = (props) => {
+  const [chart, setChart] = useState({});
+  // const [selectedBorough, setBorough] = useState('');
+
+  const api_prefix = process.env.REACT_APP_API_PROXY_URL_PREFIX;
+
+  useEffect(() => {
+    if (props.selectedBorough == null) {
+      const fetchData = async () => {
+        await fetch(`${api_prefix}boro_summary.json`).then((res) => {
+          res.json().then((data) => {
+            setChart(data);
+          });
         });
+        // .catch((error) => {
+        //   console.log(error);
+        // });
+      };
+      fetchData();
+      // console.log('inside fetch', props.selectedBorough);
+    } else {
+      switch (props.selectedBorough.value) {
+        case 2:
+          console.log("switch case bronx");
+          break;
+        case 3:
+          console.log("switch case brooklyn");
+          break;
+        case 1:
+          console.log("switch case manhattan");
+          break;
+        case 4:
+          console.log("switch case queens");
+          break;
+        case 5:
+          console.log("switch case staten island");
+          break;
+        default:
+          console.log("nothin here!");
+      }
+    }
+  }, [api_prefix, props]);
 
-        this.state.borough.forEach((boro) => {
-          console.log('boro', boro);
-        });
-      });
-  }
+  console.log("chart: ", chart);
+  // console.log('borough', selectedBorough);
 
-  setData() {
-    this.setState({
-      data: {
-        labels: ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'Staten Island'],
-        datasets: [
-          {
-            label: 'Collisions',
-            data: this.state.borough?.map((x) => x.collisions),
-            // data: [1, 2, 3, 4, 5],
-            backgroundColor: [
-              'rgba(100,178,71,0.2)', //MN
-              'rgba(192,120,98, 0.2)', //BX
-              'rgba(98,	123, 193, 0.2)', //BK
-              'rgba(192, 98, 170, 0.2)', //QN
-              'rgba(192, 167,	98, 0.2)', //SI
-            ],
-            borderColor: [
-              'rgba(100,178,71, 1)',
-              'rgba(192,120,98, 1)',
-              'rgba(98,	123, 193, 1)',
-              'rgba(192, 98, 170, 1)',
-              'rgba(192, 167,	98, 1)',
-            ],
-            borderWidth: 1,
-          },
+  var data = {
+    labels: ["Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"],
+    datasets: [
+      {
+        label: "Collisions",
+        // data: chart?.map((x) => x.collisions),
+        data: [1, 2, 3, 4, 5],
+        backgroundColor: [
+          "rgba(100,178,71,0.2)", //MN
+          "rgba(192,120,98, 0.2)", //BX
+          "rgba(98,	123, 193, 0.2)", //BK
+          "rgba(192, 98, 170, 0.2)", //QN
+          "rgba(192, 167,	98, 0.2)", //SI
         ],
+        borderColor: [
+          "rgba(100,178,71, 1)",
+          "rgba(192,120,98, 1)",
+          "rgba(98,	123, 193, 1)",
+          "rgba(192, 98, 170, 1)",
+          "rgba(192, 167,	98, 1)",
+        ],
+        borderWidth: 1,
       },
-    });
-  }
-
-  render() {
-    return (
+    ],
+  };
+  return (
       <div>
         <Bar
-          //   data={this.state.data}
-          data={{
-            labels: [
-              'Manhattan',
-              'Bronx',
-              'Brooklyn',
-              'Queens',
-              'Staten Island',
-            ],
-            datasets: [
-              {
-                label: 'Collisions',
-                data: this.state.borough?.map((x) => x.collision),
-                // data: [1, 2, 3, 4, 5],
-                backgroundColor: [
-                  'rgba(100,178,71,0.2)', //MN
-                  'rgba(192,120,98, 0.2)', //BX
-                  'rgba(98,	123, 193, 0.2)', //BK
-                  'rgba(192, 98, 170, 0.2)', //QN
-                  'rgba(192, 167,	98, 0.2)', //SI
-                ],
-                borderColor: [
-                  'rgba(100,178,71, 1)',
-                  'rgba(192,120,98, 1)',
-                  'rgba(98,	123, 193, 1)',
-                  'rgba(192, 98, 170, 1)',
-                  'rgba(192, 167,	98, 1)',
-                ],
-                borderWidth: 1,
-              },
-            ],
-          }}
-          height={550}
-          width={600}
-          //   options={{
-          //     maintainAspectRatio: false,
-          //   }}
+            data={data}
+            height={300}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+            }}
         />
       </div>
-    );
-  }
-}
+  );
+};
 
 export default BarChart;
